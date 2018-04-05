dabfe963e070620f9da6d3a470aae0c9289bb9fd
Gathika94 <gathika.14@cse.mrt.ac.lk>
2018-04-05 14:51:17 +0530

    minor fixes for wod similarity, examples added

--- sentence-feature-extractor/src/main/java/featureextractor/examples/CosineWordSimilarity.java
+++ sentence-feature-extractor/src/main/java/featureextractor/examples/CosineWordSimilarity.java
@@ -1,12 +1,15 @@
 package featureextractor.examples;
 
+import featureextractor.cosinesimilarity.Similarity;
 import featureextractor.cosinesimilarity.WordSimilarity;
 
 public class CosineWordSimilarity {
 
     public static void main(String[] args) {
-        WordSimilarity wordSimilarity = new WordSimilarity();
-        double scoreWordsSimilarity = wordSimilarity.cosine_word_similarity_score(
+        Similarity similarity = new WordSimilarity(
+                "Julie loves me more than Linda loves me",
+                "Jane likes me more than Julie loves me") ;
+        double scoreWordsSimilarity = similarity.similarityScore(
                 "Julie loves me more than Linda loves me",
                 "Jane likes me more than Julie loves me"
                 );
