e41dc386f7a6f35644c5846af08d1cb1888f9b80
Gathika94 <gathika.14@cse.mrt.ac.lk>
2018-04-20 16:49:56 +0530

    fixes issues in semantic sentence similarity

--- sentence-feature-extractor/src/main/java/featureextractor/semanticsimilarity/SemanticSentenceSimilarity.java
+++ sentence-feature-extractor/src/main/java/featureextractor/semanticsimilarity/SemanticSentenceSimilarity.java
@@ -1,5 +1,9 @@
 package featureextractor.semanticsimilarity;
 
+import edu.cmu.lti.jawjaw.pobj.POS;
+import edu.cmu.lti.lexical_db.data.Concept;
+import edu.cmu.lti.ws4j.Relatedness;
+import edu.cmu.lti.ws4j.RelatednessCalculator;
 import edu.stanford.nlp.pipeline.Annotation;
 import utils.NLPUtils;
 import edu.cmu.lti.lexical_db.ILexicalDatabase;
@@ -9,6 +13,7 @@ import edu.cmu.lti.ws4j.util.WS4JConfiguration;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class SemanticSentenceSimilarity {
@@ -27,6 +32,7 @@ public class SemanticSentenceSimilarity {
 
     NLPUtils nlpUtils;
     private static ILexicalDatabase db = new NictWordNet();
+    static RelatednessCalculator rc = new WuPalmer(db);
 
     public SemanticSentenceSimilarity(Annotation stc1, Annotation stc2,NLPUtils nlpUtls){
         annotation1 = stc1;
@@ -38,6 +44,30 @@ public class SemanticSentenceSimilarity {
         WS4JConfiguration.getInstance().setMFS(true);
         double s = new WuPalmer(db).calcRelatednessOfWords(word1, word2);
         return s;
+    }
+
+    public double wordSimilarity(String word1, POS posWord1, String word2, POS posWord2) {
+        double maxScore = 0D;
+        try {
+            WS4JConfiguration.getInstance().setMFS(true);
+            List<Concept> synsets1 = (List<Concept>) db.getAllConcepts(word1, posWord1.name());
+            List<Concept> synsets2 = (List<Concept>) db.getAllConcepts(word2, posWord2.name());
+            for (Concept synset1 : synsets1) {
+                for (Concept synset2 : synsets2) {
+                    Relatedness relatedness = rc.calcRelatednessOfSynset(synset1, synset2);
+                    double score = relatedness.getScore();
+                    if (score > maxScore) {
+                        maxScore = score;
+                    }
+                }
+            }
+          /*  System.out.println("s1 : "+synsets1.toString());
+            System.out.println("s2 : "+synsets2.toString());
+            System.out.println("Similarity score of " + word1 + " & " + word2 + " : " + maxScore);*/
+        } catch (Exception e) {
+            System.out.println(("Exception : "+ e));
+        }
+        return maxScore;
     }
 
     public void getVerbsAndNouns(){
@@ -56,7 +86,15 @@ public class SemanticSentenceSimilarity {
         getVerbsAndNouns();
         for(int i=0; i<nouns.size()-1; i++){
             for(int j=i+1; j<nouns.size(); j++){
-                double distance = compute(nouns.get(i), nouns.get(j));
+                double distance;
+                if(nouns.get(i).equals(nouns.get(j))){
+                    distance = 1;
+                }else {
+
+                    distance = wordSimilarity(nouns.get(i), POS.n, nouns.get(j), POS.n);
+
+                }
+                        //compute(nouns.get(i), nouns.get(j));
                 aggregateNounDistance+= distance;
                 System.out.println(nouns.get(i) +" -  " +  nouns.get(j) + " = " + distance);
                 nounCount++;
@@ -65,7 +103,15 @@ public class SemanticSentenceSimilarity {
         }
         for(int i=0; i<verbs.size()-1; i++){
             for(int j=i+1; j<verbs.size(); j++){
-                double distance = compute(verbs.get(i), verbs.get(j));
+
+                double distance;
+                if(verbs.get(i).equals(verbs.get(j))){
+                    distance = 1;
+                }else {
+                    distance = wordSimilarity(nouns.get(i), POS.v, nouns.get(j), POS.v);
+
+                    //compute(verbs.get(i), verbs.get(j));
+                }
                 aggregateVerbDistance+=distance;
                 System.out.println(verbs.get(i) +" -  " +  verbs.get(j) + " = " + distance);
                 verbCount++;
