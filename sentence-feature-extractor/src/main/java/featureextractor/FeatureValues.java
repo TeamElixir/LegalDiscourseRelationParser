a9ae2d3e310210ace63e12e3ddd63f71754704ec
Gathika94 <gathika.14@cse.mrt.ac.lk>
2018-04-21 05:39:38 +0530

    method to calculate correlation coefficient

--- sentence-feature-extractor/src/main/java/featureextractor/FeatureValues.java
+++ sentence-feature-extractor/src/main/java/featureextractor/FeatureValues.java
@@ -2,6 +2,9 @@ package featureextractor;
 
 import java.util.ArrayList;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.stat.correlation.*;
+
 public class FeatureValues {
     private ArrayList<Double> wordSimilarity = new ArrayList<>();
     private ArrayList<Double> semanticSimilarityScore = new ArrayList<>();
@@ -10,11 +13,18 @@ public class FeatureValues {
     private Double[] semanticSimilarityArray;
 
     public void initializeArrays(){
+
         wordSimilarityArray= wordSimilarity.toArray(new Double[wordSimilarity.size()]);
         semanticSimilarityArray = semanticSimilarityScore.toArray(new Double[semanticSimilarityScore.size()]);
     }
 
     public void calculateCoreference(){
+        initializeArrays();
+        System.out.print("Pearson\'s coefficient of correlation: ");
+        PearsonsCorrelation pc = new PearsonsCorrelation();
+        double cc = pc.correlation(ArrayUtils.toPrimitive(wordSimilarityArray),
+                ArrayUtils.toPrimitive(semanticSimilarityArray));
+        System.out.println(cc);
 
     }
 
