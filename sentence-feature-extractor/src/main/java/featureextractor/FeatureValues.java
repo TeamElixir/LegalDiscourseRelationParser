2464c248f83758aeaaddb793e24f3584a7c26388
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2018-04-21 17:46:07 +0530

    Correlation calculation

--- sentence-feature-extractor/src/main/java/featureextractor/FeatureValues.java
+++ sentence-feature-extractor/src/main/java/featureextractor/FeatureValues.java
@@ -2,46 +2,64 @@ package featureextractor;
 
 import java.util.ArrayList;
 
+import datasetparser.models.FeatureEntryDB;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.stat.correlation.*;
 
 public class FeatureValues {
-    private ArrayList<Double> wordSimilarity = new ArrayList<>();
-    private ArrayList<Double> semanticSimilarityScore = new ArrayList<>();
+    private ArrayList<Double> feature1List = new ArrayList<>();
+    private ArrayList<Double> feature2List = new ArrayList<>();
 
-    private Double[] wordSimilarityArray ;
-    private Double[] semanticSimilarityArray;
+    private Double[] feature1Array;
+    private Double[] feature2Array;
 
     public void initializeArrays(){
 
-        wordSimilarityArray= wordSimilarity.toArray(new Double[wordSimilarity.size()]);
-        semanticSimilarityArray = semanticSimilarityScore.toArray(new Double[semanticSimilarityScore.size()]);
+        feature1Array = feature1List.toArray(new Double[feature1List.size()]);
+        feature2Array = feature2List.toArray(new Double[feature2List.size()]);
     }
 
-    public void calculateCoreference(){
+    public double calculateCorrelation(){
         initializeArrays();
-        System.out.print("Pearson\'s coefficient of correlation: ");
         PearsonsCorrelation pc = new PearsonsCorrelation();
-        double cc = pc.correlation(ArrayUtils.toPrimitive(wordSimilarityArray),
-                ArrayUtils.toPrimitive(semanticSimilarityArray));
-        System.out.println(cc);
+        double cc = pc.correlation(ArrayUtils.toPrimitive(feature1Array),
+                ArrayUtils.toPrimitive(feature2Array));
 
+		return cc;
     }
 
-
-    public void setWordSimilarity(ArrayList<Double> wordSimilarity) {
-        this.wordSimilarity = wordSimilarity;
+    public void setFeature1List(ArrayList<Double> feature1List) {
+        this.feature1List = feature1List;
     }
 
-    public void setSemanticSimilarityScore(ArrayList<Double> semanticSimilarityScore) {
-        this.semanticSimilarityScore = semanticSimilarityScore;
+    public void setFeature2List(ArrayList<Double> feature2List) {
+        this.feature2List = feature2List;
     }
 
-    public ArrayList<Double> getWordSimilarity() {
-        return wordSimilarity;
+    public ArrayList<Double> getFeature1List() {
+        return feature1List;
     }
 
-    public ArrayList<Double> getSemanticSimilarityScore() {
-        return semanticSimilarityScore;
+    public ArrayList<Double> getFeature2List() {
+        return feature2List;
     }
+
+	public static void main(String[] args) throws Exception{
+		ArrayList<FeatureEntryDB> featureEntryDBS = FeatureEntryDB.getAll();
+
+		ArrayList<Double> feature1 = new ArrayList<>();
+		ArrayList<Double> feature2 = new ArrayList<>();
+
+		for(FeatureEntryDB entry: featureEntryDBS){
+			feature1.add(entry.getWordSimilarity());
+			feature2.add(entry.getSemanticSimilarityScore());
+		}
+
+		FeatureValues featureValues = new FeatureValues();
+		featureValues.setFeature1List(feature1);
+		featureValues.setFeature2List(feature2);
+
+		System.out.println("Pearson\'s coefficient of correlation: " + featureValues.calculateCorrelation());
+
+	}
 }
