bb2706ad4557c9c4a6f29c97eb4401d6ddf6ffb1
Gathika94 <gathika.14@cse.mrt.ac.lk>
2018-04-21 20:17:08 +0530

    svm example is working

--- svm-implementation/src/main/java/aa.java
+++ svm-implementation/src/main/java/aa.java
@@ -1,7 +1,32 @@
 import libsvm.*;
 
 public class aa {
-     private svm_model svmTrain() {
+     static double[][] train = new double[1000][];
+     static double[][] test = new double[3][];
+
+     public static void main(String[] args) {
+          double[] featureTest1 = {1,0,600};
+          test[0] = featureTest1;
+          double[] featureTest2 = {1,0,700};
+          test[1] = featureTest2;
+          double[] featureTest3 = {0,0,-800};
+          test[2] = featureTest3;
+
+          for (int i = 0; i < train.length; i++){
+               if (i+1 > (train.length/2)){        // 50% positive
+                    double[] vals = {1,0,i+i};
+                    train[i] = vals;
+               } else {
+                    double[] vals = {0,0,i-i-i-2}; // 50% negative
+                    train[i] = vals;
+               }
+          }
+          svm_model svmModel = svmTrain();
+          double vv = evaluate(featureTest1,svmModel);
+
+     }
+
+     private static svm_model svmTrain() {
           svm_problem prob = new svm_problem();
           int dataCount = train.length;
           prob.y = new double[dataCount];
@@ -33,5 +58,32 @@ public class aa {
           svm_model model = svm.svm_train(prob, param);
 
           return model;
+     }
+
+     public static double evaluate(double[] features, svm_model model)
+     {
+          svm_node[] nodes = new svm_node[features.length-1];
+          for (int i = 1; i < features.length; i++)
+          {
+               svm_node node = new svm_node();
+               node.index = i;
+               node.value = features[i];
+
+               nodes[i-1] = node;
+          }
+
+          int totalClasses = 2;
+          int[] labels = new int[totalClasses];
+          svm.svm_get_labels(model,labels);
+
+          double[] prob_estimates = new double[totalClasses];
+          double v = svm.svm_predict_probability(model, nodes, prob_estimates);
+
+          for (int i = 0; i < totalClasses; i++){
+               System.out.print("(" + labels[i] + ":" + prob_estimates[i] + ")");
+          }
+          System.out.println("(Actual:" + features[0] + " Prediction:" + v + ")");
+
+          return v;
      }
 }
