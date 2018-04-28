e505c3c7223c43c42c37435369a85309220b837d
Gathika94 <gathika.14@cse.mrt.ac.lk>
2018-04-28 22:10:33 +0530

    new ellaboration phrases and words added

--- sentence-feature-extractor/src/main/java/featureextractor/sentencepropertyfeatures/PropertyUtils.java
+++ sentence-feature-extractor/src/main/java/featureextractor/sentencepropertyfeatures/PropertyUtils.java
@@ -20,23 +20,33 @@ public class PropertyUtils {
                     "in fact","in general", "including this","in this case","for example","for this reason","for instance",
                     "to put it", "to demonstrate this", "that is to say", "on the positive side",
                      "with this in mind", "as a result",
-                    "in that case", "in effect"
+                    "in that case", "in effect", "one is","two is","Another is","Neither this", "each of these",
+                    "in reaching this","as relevant here","it follows","in those cases",
+                    "either way","to do so","to achieve that","to perform that",
+                    "to show that","in each case”","at that time","this opinion","confirms this","confirm this",
+                    "in context","the statute’s","that phrase","one is","second is","third is",
+                    "another is","they are","that is","with these assumptions", " special factors here",
+                    "other special factors","these reasons", "these arguments"
+
 
             ));
 
     private static final ArrayList<String> ellaborationWords = new ArrayList<String>(
             Arrays.asList( "again", "to", "moreover", "more","and", "also","then", "likewise",
                     "equally","comparatively", "identically","correspondingly","first","uniquely",
-                    "similarly", "like", "furthermore", "as", "additionally","too","although" ,
+                    "similarly", "like", "furthermore", "as", "additionally","too",
                     "but","still", "instead","unlike","whereas", "despite","yet", "conversely",
                     "otherwise","nevertheless","besides", "rather","nonetheless","regardless","above",
                     "notwithstanding","that", "those", "such","indeed","especially","consequently", "thus",
-                    "therefore","thereupon","forthwith","hence", "accordingly", "henceforth")
-    );
+                    "therefore","thereupon","forthwith","hence", "accordingly", "henceforth", "later","these",
+                    "see","subsequent","nor","finally","accordingly","second","third","fourth"
+
+                    )
+    ); //although is removed
 
     private static final ArrayList<String> changeOfTopicsWords = new ArrayList<String>(
             Arrays.asList("in contrast","on the contrary","on the negative side",
-                    "however","albeit"
+                    "however","albeit","meanwhile"
                     )
     );
 
