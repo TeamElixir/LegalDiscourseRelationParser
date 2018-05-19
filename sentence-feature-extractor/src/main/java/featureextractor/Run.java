5a1d0ca6c213d1b50bfc9b6a1ef9c0b97632bfb9
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2018-05-19 07:21:40 +0530

    semantic_score * 2

---
+++ sentence-feature-extractor/src/main/java/featureextractor/Run.java
@@ -0,0 +1,32 @@
+package featureextractor;
+
+import java.util.ArrayList;
+
+import datasetparser.models.FeatureEntry;
+import utils.SQLiteUtils;
+
+public class Run {
+
+	public static void main(String[] args) throws Exception {
+		ArrayList<FeatureEntry> featureEntries = FeatureEntry.getAll();
+		SQLiteUtils sqLiteUtils = new SQLiteUtils();
+
+		for (int i = 0; i < featureEntries.size(); i++) {
+			double semScore = featureEntries.get(i).getSemanticSimilarityScore();
+
+			semScore = semScore * 2.0;
+			if (semScore > 1) {
+				semScore = 1;
+			}
+
+			String sql = "UPDATE FEATURE_ENTRY SET SEMANTIC_SCORE=" + semScore +
+					" WHERE ID=" +featureEntries.get(i).getDbId();
+
+			sqLiteUtils.executeUpdate(sql);
+//			System.out.println(i);
+
+		}
+
+	}
+
+}
