5e9823ad52622a7c3b3c400f870dfc1053af90ef
Gathika94 <gathika.14@cse.mrt.ac.lk>
2018-07-20 12:39:23 +0530

    adverbs types

---
+++ sentence-feature-extractor/src/main/java/shiftinview/utils/ModifierUtils.java
@@ -0,0 +1,25 @@
+package shiftinview.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ModifierUtils {
+
+    private static ArrayList<String> lessFrequent=new ArrayList<String>(
+            Arrays.asList("accidentally","never","not","less", "loosely","rarely" , "sometimes"));
+    private static  ArrayList<String> moreFrequent = new ArrayList<String>(
+            Arrays.asList("always","often","repeatedly", "regularly"));
+
+    private static ArrayList<String>  amplifiers = new ArrayList<String>(
+            Arrays.asList("completely","absolutely","heartily","so","well","really","literally",
+                    "simply","for sure"));
+    private static ArrayList<String> downtoners = new ArrayList<String>(
+            Arrays.asList("kind of", "sort of", "mildly", "to some extent", "almost", "all but")
+    );
+
+    private static ArrayList<String> positiveManner = new ArrayList<String>(
+            Arrays.asList("elegantly","beautifully")
+    );
+
+
+}
