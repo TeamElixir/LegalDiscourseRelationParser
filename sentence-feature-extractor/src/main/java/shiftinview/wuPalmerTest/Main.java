7ed5fef4e818c6a02349e7939f90fc6151502478
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-09-04 01:09:10 +0530

    feat(wu-palmer-test): count pairs with common verbs

--- sentence-feature-extractor/src/main/java/shiftinview/wuPalmerTest/Main.java
+++ sentence-feature-extractor/src/main/java/shiftinview/wuPalmerTest/Main.java
@@ -13,15 +13,21 @@ public class Main {
         NLPUtils nlpUtils = new NLPUtils("tokenize,ssplit,pos");
         int i = 0;
         for (SentencePair sentencePair : allSentencePairs) {
-            if (i++ == 9) break;
             ArrayList<String> sourceVerbs = getSourceVerbs(sentencePair.getSourceSentence(), nlpUtils);
-            System.out.println(sourceVerbs);
-
             ArrayList<String> targetVerbs = getTargetVerbs(sentencePair.getTargetSentence(), nlpUtils);
-            System.out.println(targetVerbs);
 
-            System.out.println("");
+            ArrayList<String> common = new ArrayList<String>(sourceVerbs);
+            common.retainAll(targetVerbs);
+
+            if (common.size() > 0) {
+                System.out.println("PairID: " + sentencePair.getId());
+                System.out.println(common);
+                System.out.println();
+                i++;
+            }
         }
+        System.out.print("Number of pairs with common verbs: " + i);
+        System.out.println(", out of " + allSentencePairs.size() + " pairs.");
     }
 
     private static ArrayList<String> getSourceVerbs(Sentence sourceSentence, NLPUtils nlpUtils) {
