b64919eae52b9d2c4ce2ea31d9b71fa353d6e164
ThejanRupasinghe <thejanrupasinghe.14@cse.mrt.ac.lk>
2018-09-19 15:07:33 +0530

    Adds legal ollie triples

--- sentence-feature-extractor/src/main/java/ollieparser/OllieTriple.java
+++
@@ -1,8 +0,0 @@
-package ollieparser;
-
-public class OllieTriple {
-    public double confidence;
-    public String subject;
-    public String relation;
-    public String object;
-}
---
+++ sentence-feature-extractor/src/main/java/shiftinview/models/ShiftInViewPair.java
@@ -0,0 +1,5 @@
+package shiftinview.models;
+
+public class ShiftInViewPair {
+
+}
