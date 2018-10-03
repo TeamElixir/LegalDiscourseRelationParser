cd1b67ef0e8b2a032d25ce2d5e84b59911ff5a43
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-10-03 21:43:58 +0530

    feat(Shift-In-View-Analyzer): use Lin instead of WuPalmer

--- sentence-feature-extractor/src/main/java/shiftinview/ShiftInViewAnalyzer.java
+++ sentence-feature-extractor/src/main/java/shiftinview/ShiftInViewAnalyzer.java
@@ -1,54 +1,50 @@
 package shiftinview;
 
+import edu.cmu.lti.jawjaw.pobj.POS;
 import edu.stanford.nlp.pipeline.Annotation;
 import featureextractor.semanticsimilarity.SemanticSentenceSimilarity;
-
-import shiftinview.models.VerbRelation;
 import shiftinview.models.VerbPair;
+import shiftinview.models.VerbRelation;
 import shiftinview.utils.ModifierUtils;
 import utils.NLPUtils;
 
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
-
-import edu.cmu.lti.jawjaw.pobj.POS;
 
 public class ShiftInViewAnalyzer {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String targetSentence = "Lee could not show that he was prejudiced by his attorney's erroneous advice.";
-		String sourceSentence
-				= "Lee has demonstrated that he was prejudiced by his counsel's erroneous advice.";
+        String targetSentence = "Lee could not show that he was prejudiced by his attorney's erroneous advice.";
+        String sourceSentence
+                = "Lee has demonstrated that he was prejudiced by his counsel's erroneous advice.";
 
-		// set up pipeline properties
-		Properties props = new Properties();
-		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse");
-		// use faster shift reduce parser
-		props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");
-		props.setProperty("parse.maxlen", "100");
-		// set up Stanford CoreNLP pipeline
-		// StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        // set up pipeline properties
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse");
+        // use faster shift reduce parser
+        props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");
+        props.setProperty("parse.maxlen", "100");
+        // set up Stanford CoreNLP pipeline
+        // StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-		NLPUtils nlpUtils = new NLPUtils(props);
-		checkRelationsForOppositeness(nlpUtils, targetSentence, sourceSentence);
-	}
+        NLPUtils nlpUtils = new NLPUtils(props);
+        checkRelationsForOppositeness(nlpUtils, targetSentence, sourceSentence);
+    }
 
-	public static Integer checkRelationsForOppositeness(NLPUtils nlpUtils, String targetSentence, String sourceSentence) {
+    public static Integer checkRelationsForOppositeness(NLPUtils nlpUtils, String targetSentence, String sourceSentence) {
 
-		ArrayList<VerbRelation> verbsSentence1;
-		ArrayList<VerbRelation> verbsSentence2;
+        ArrayList<VerbRelation> verbsSentence1;
+        ArrayList<VerbRelation> verbsSentence2;
 
-		Annotation targetAnnotation;
-		Annotation sourceAnnotation;
+        Annotation targetAnnotation;
+        Annotation sourceAnnotation;
 
-		ArrayList<String> coreferencedSentences = new ArrayList<>();
+        ArrayList<String> coreferencedSentences = new ArrayList<>();
 
-		// build annotation for a review
-		sourceAnnotation = nlpUtils.annotate(sourceSentence);
-		targetAnnotation = nlpUtils.annotate(targetSentence);
+        // build annotation for a review
+        sourceAnnotation = nlpUtils.annotate(sourceSentence);
+        targetAnnotation = nlpUtils.annotate(targetSentence);
 
 
 
@@ -56,198 +52,200 @@ public class ShiftInViewAnalyzer {
         coreferencedSentences=Coreferencer.getCoreferencedSentences(sourceSentence,targetSentence,nlpUtils);
         System.out.println(coreferencedSentences.get(0));
         System.out.println(coreferencedSentences.get(1));*/
-		ConstituentParser constituentParser = new ConstituentParser();
-		//  constituentParser.runConstituentParser(sourceAnnotation,nlpUtils);
+        ConstituentParser constituentParser = new ConstituentParser();
+        //  constituentParser.runConstituentParser(sourceAnnotation,nlpUtils);
 
-		System.out.println("break");
+        System.out.println("break");
 
-		verbsSentence1 = constituentParser.getVerbRelationships(sourceAnnotation, nlpUtils);
+        verbsSentence1 = constituentParser.getVerbRelationships(sourceAnnotation, nlpUtils);
 
-		System.out.println("targetSentence");
-		System.out.println(" ");
+        System.out.println("targetSentence");
+        System.out.println(" ");
 
-		verbsSentence2 = constituentParser.getVerbRelationships(targetAnnotation, nlpUtils);
-		String verbSource = "";
-		String verbTarget = "";
-		Integer verbSourceId = -1;
-		Integer verbTargetId = -1;
-		String sourceOther = "";
-		String targetOther = "";
-		VerbRelation verbObjectTarget = new VerbRelation();
-		VerbRelation verbObjectSource = new VerbRelation();
-		String currentSourceVerb = "";
-		String currentTargetVerb = "";
-		int currentSourceVerbID = -1;
-		int currentTargetVerbID = -1;
-		VerbRelation secondverb;
-		VerbRelation verb;
-		ArrayList<VerbPair> closeVerbs = new ArrayList<>();
+        verbsSentence2 = constituentParser.getVerbRelationships(targetAnnotation, nlpUtils);
+        String verbSource = "";
+        String verbTarget = "";
+        Integer verbSourceId = -1;
+        Integer verbTargetId = -1;
+        String sourceOther = "";
+        String targetOther = "";
+        VerbRelation verbObjectTarget = new VerbRelation();
+        VerbRelation verbObjectSource = new VerbRelation();
+        String currentSourceVerb = "";
+        String currentTargetVerb = "";
+        int currentSourceVerbID = -1;
+        int currentTargetVerbID = -1;
+        VerbRelation secondverb;
+        VerbRelation verb;
+        ArrayList<VerbPair> closeVerbs = new ArrayList<>();
 
-		System.out.println("array sizes");
-		System.out.println(verbsSentence2.size());
-		System.out.println(verbsSentence1.size());
+        System.out.println("array sizes");
+        System.out.println(verbsSentence2.size());
+        System.out.println(verbsSentence1.size());
 
-		for (int j = 0; j < verbsSentence2.size(); j++) {
-			verb = verbsSentence2.get(j);
+        for (int j = 0; j < verbsSentence2.size(); j++) {
+            verb = verbsSentence2.get(j);
 
-			if (verb.isVerbIsDep()) {
-				verbTarget = verb.getDepLemma();
-				verbTargetId = verb.getId();
-				verbObjectTarget = verb;
-			} else if (verb.isVerbIsGov()) {
-				verbTarget = verb.getGovLemma();
-				verbTargetId = verb.getId();
-				verbObjectTarget = verb;
-			}
-			for (int i = 0; i < verbsSentence1.size(); i++) {
-				secondverb = verbsSentence1.get(i);
+            if (verb.isVerbIsDep()) {
+                verbTarget = verb.getDepLemma();
+                verbTargetId = verb.getId();
+                verbObjectTarget = verb;
+            } else if (verb.isVerbIsGov()) {
+                verbTarget = verb.getGovLemma();
+                verbTargetId = verb.getId();
+                verbObjectTarget = verb;
+            }
+            for (int i = 0; i < verbsSentence1.size(); i++) {
+                secondverb = verbsSentence1.get(i);
 
-				if (secondverb.isVerbIsDep()) {
+                if (secondverb.isVerbIsDep()) {
 
-					verbSource = secondverb.getDepLemma();
-					verbSourceId = verb.getId();
-					verbObjectSource = secondverb;
-				} else if (secondverb.isVerbIsGov()) {
-					verbSource = secondverb.getGovLemma();
-					verbSourceId = verb.getId();
-					verbObjectSource = secondverb;
-				}
+                    verbSource = secondverb.getDepLemma();
+                    verbSourceId = verb.getId();
+                    verbObjectSource = secondverb;
+                } else if (secondverb.isVerbIsGov()) {
+                    verbSource = secondverb.getGovLemma();
+                    verbSourceId = verb.getId();
+                    verbObjectSource = secondverb;
+                }
 
 
                 /*System.out.println(" ");
                 System.out.println("s  "  +verbSource);
                 System.out.println("t  "+ verbTarget);*/
-				SemanticSentenceSimilarity semanticSentenceSimilarity = new SemanticSentenceSimilarity();
-				double score = semanticSentenceSimilarity.wordSimilarity(verbSource, POS.v, verbTarget, POS.v);
-				if (score >= 0.88) {
-					if (currentSourceVerbID != verbSourceId || currentTargetVerbID != verbTargetId) {
+                SemanticSentenceSimilarity semanticSentenceSimilarity = new SemanticSentenceSimilarity();
+//				double score = semanticSentenceSimilarity.wordSimilarity(verbSource, POS.v, verbTarget, POS.v);
+                // using Lin instead of WuPalmer
+                double score = semanticSentenceSimilarity.getAllWordSimilarityScores(verbSource, POS.v, verbTarget, POS.v)[4];
+                if (score >= 0.86) {
+                    if (currentSourceVerbID != verbSourceId || currentTargetVerbID != verbTargetId) {
                         /*String[] verbPair = new String[2];
                         verbPair[0]=verbTarget;
                         verbPair[1]=verbSource;*/
-						currentSourceVerbID = verbSourceId;
-						currentTargetVerbID = verbTargetId;
-						//System.out.println("rel :"+verbObjectTarget.getRelation());
-						//System.out.println("rel :" + verbObjectSource.getRelation());
+                        currentSourceVerbID = verbSourceId;
+                        currentTargetVerbID = verbTargetId;
+                        //System.out.println("rel :"+verbObjectTarget.getRelation());
+                        //System.out.println("rel :" + verbObjectSource.getRelation());
 
-						if (verbObjectTarget.isVerbIsDep()) {
-							//System.out.println("other :"+verbObjectTarget.getGovWord());
-							targetOther = verbObjectTarget.getGovWord();
-						} else if (verbObjectTarget.isVerbIsGov()) {
-							//System.out.println("other :"+verbObjectTarget.getDepWord());
-							targetOther = verbObjectTarget.getDepWord();
-						}
-						if (verbObjectSource.isVerbIsDep()) {
-							//System.out.println("other :"+verbObjectSource.getGovWord());
-							sourceOther = verbObjectSource.getGovWord();
-						} else if (verbObjectSource.isVerbIsGov()) {
-							//System.out.println("other :"+verbObjectSource.getDepWord());
-							sourceOther = verbObjectSource.getDepWord();
-						}
-						VerbPair verbPair = new VerbPair();
-						verbPair.setTargetVerb(verbTarget);
-						verbPair.setSourceVerb(verbSource);
-						if (verbObjectTarget.getRelation().equals("neg")) {
-							verbPair.setTargetVerbNegated(true);
-						}
-						if (verbObjectSource.getRelation().equals("neg")) {
-							verbPair.setSourceVerbNegated(true);
-						}
-						if (ModifierUtils.getNegativeWords().contains(targetOther)) {
-							verbPair.setTargetVerbNegated(true);
-						}
-						if (ModifierUtils.getNegativeWords().contains(sourceOther)) {
-							verbPair.setSourceVerbNegated(true);
-						}
+                        if (verbObjectTarget.isVerbIsDep()) {
+                            //System.out.println("other :"+verbObjectTarget.getGovWord());
+                            targetOther = verbObjectTarget.getGovWord();
+                        } else if (verbObjectTarget.isVerbIsGov()) {
+                            //System.out.println("other :"+verbObjectTarget.getDepWord());
+                            targetOther = verbObjectTarget.getDepWord();
+                        }
+                        if (verbObjectSource.isVerbIsDep()) {
+                            //System.out.println("other :"+verbObjectSource.getGovWord());
+                            sourceOther = verbObjectSource.getGovWord();
+                        } else if (verbObjectSource.isVerbIsGov()) {
+                            //System.out.println("other :"+verbObjectSource.getDepWord());
+                            sourceOther = verbObjectSource.getDepWord();
+                        }
+                        VerbPair verbPair = new VerbPair();
+                        verbPair.setTargetVerb(verbTarget);
+                        verbPair.setSourceVerb(verbSource);
+                        if (verbObjectTarget.getRelation().equals("neg")) {
+                            verbPair.setTargetVerbNegated(true);
+                        }
+                        if (verbObjectSource.getRelation().equals("neg")) {
+                            verbPair.setSourceVerbNegated(true);
+                        }
+                        if (ModifierUtils.getNegativeWords().contains(targetOther)) {
+                            verbPair.setTargetVerbNegated(true);
+                        }
+                        if (ModifierUtils.getNegativeWords().contains(sourceOther)) {
+                            verbPair.setSourceVerbNegated(true);
+                        }
 
-						if (ModifierUtils.getLessFrequent().contains(targetOther)) {
-							verbPair.setLessFrequentTarget(true);
-						}
-						if (ModifierUtils.getMoreFrequent().contains(targetOther)) {
-							verbPair.setMoreFrequentTarget(true);
-						}
-						if (ModifierUtils.getDowntoners().contains(targetOther)) {
-							verbPair.setDowntonerTarget(true);
-						}
-						if (ModifierUtils.getAmplifiers().contains(targetOther)) {
-							verbPair.setAmplifierTarget(true);
-						}
-						if (ModifierUtils.getNegativeManner().contains(targetOther)) {
-							verbPair.setNegativeMannerTarget(true);
-						}
-						if (ModifierUtils.getPositiveManner().contains(targetOther)) {
-							verbPair.setPositiveMannerTarget(true);
-						}
+                        if (ModifierUtils.getLessFrequent().contains(targetOther)) {
+                            verbPair.setLessFrequentTarget(true);
+                        }
+                        if (ModifierUtils.getMoreFrequent().contains(targetOther)) {
+                            verbPair.setMoreFrequentTarget(true);
+                        }
+                        if (ModifierUtils.getDowntoners().contains(targetOther)) {
+                            verbPair.setDowntonerTarget(true);
+                        }
+                        if (ModifierUtils.getAmplifiers().contains(targetOther)) {
+                            verbPair.setAmplifierTarget(true);
+                        }
+                        if (ModifierUtils.getNegativeManner().contains(targetOther)) {
+                            verbPair.setNegativeMannerTarget(true);
+                        }
+                        if (ModifierUtils.getPositiveManner().contains(targetOther)) {
+                            verbPair.setPositiveMannerTarget(true);
+                        }
 
-						if (ModifierUtils.getLessFrequent().contains(sourceOther)) {
-							verbPair.setLessFrequentSource(true);
-						}
-						if (ModifierUtils.getMoreFrequent().contains(sourceOther)) {
-							verbPair.setMoreFrequentSource(true);
-						}
-						if (ModifierUtils.getDowntoners().contains(sourceOther)) {
-							verbPair.setDowntonerSource(true);
-						}
-						if (ModifierUtils.getAmplifiers().contains(sourceOther)) {
-							verbPair.setAmplifierSource(true);
-						}
-						if (ModifierUtils.getNegativeManner().contains(sourceOther)) {
-							verbPair.setNegativeMannerSource(true);
-						}
-						if (ModifierUtils.getPositiveManner().contains(sourceOther)) {
-							verbPair.setPositiveMannerSource(true);
-						}
+                        if (ModifierUtils.getLessFrequent().contains(sourceOther)) {
+                            verbPair.setLessFrequentSource(true);
+                        }
+                        if (ModifierUtils.getMoreFrequent().contains(sourceOther)) {
+                            verbPair.setMoreFrequentSource(true);
+                        }
+                        if (ModifierUtils.getDowntoners().contains(sourceOther)) {
+                            verbPair.setDowntonerSource(true);
+                        }
+                        if (ModifierUtils.getAmplifiers().contains(sourceOther)) {
+                            verbPair.setAmplifierSource(true);
+                        }
+                        if (ModifierUtils.getNegativeManner().contains(sourceOther)) {
+                            verbPair.setNegativeMannerSource(true);
+                        }
+                        if (ModifierUtils.getPositiveManner().contains(sourceOther)) {
+                            verbPair.setPositiveMannerSource(true);
+                        }
 
-						closeVerbs.add(verbPair);
+                        closeVerbs.add(verbPair);
 
-					}
-				}
+                    }
+                }
 
-				//System.out.println("score: "+score);
+                //System.out.println("score: "+score);
 
-			}
+            }
 
-		}
-		Integer value = detectNegation(closeVerbs);
-		return value;
+        }
+        Integer value = detectNegation(closeVerbs);
+        return value;
 
-	}
+    }
 
-	public static Integer detectNegation(ArrayList<VerbPair> closeVerbs) {
-		System.out.println("close verbs");
-		Integer count = 0;
+    public static Integer detectNegation(ArrayList<VerbPair> closeVerbs) {
+        System.out.println("close verbs");
+        Integer count = 0;
 
-		for (VerbPair pair : closeVerbs) {
-			System.out.println("TV :" + pair.getTargetVerb());
-			System.out.println("SV :" + pair.getSourceVerb());
-			System.out.println("targetNegated: " + pair.getTargetVerbNegated());
-			System.out.println("sourceNegated: " + pair.getSourceVerbNegated());
-			if (pair.getTargetVerbNegated()) {
-				if (!pair.getSourceVerbNegated()) {
-					count++;
-				}
-			} else {
-				if (pair.getSourceVerbNegated()) {
-					count++;
-				}
-			}
-			if (pair.getLessFrequentTarget() && pair.getMoreFrequentSource() ||
-					pair.getMoreFrequentTarget() && pair.getLessFrequentSource()) {
-				count++;
+        for (VerbPair pair : closeVerbs) {
+            System.out.println("TV :" + pair.getTargetVerb());
+            System.out.println("SV :" + pair.getSourceVerb());
+            System.out.println("targetNegated: " + pair.getTargetVerbNegated());
+            System.out.println("sourceNegated: " + pair.getSourceVerbNegated());
+            if (pair.getTargetVerbNegated()) {
+                if (!pair.getSourceVerbNegated()) {
+                    count++;
+                }
+            } else {
+                if (pair.getSourceVerbNegated()) {
+                    count++;
+                }
+            }
+            if (pair.getLessFrequentTarget() && pair.getMoreFrequentSource() ||
+                    pair.getMoreFrequentTarget() && pair.getLessFrequentSource()) {
+                count++;
 
-			}
-			if (pair.getAmplifierTarget() && pair.getDowntonerSource() ||
-					pair.getDowntonerTarget() && pair.getAmplifierSource()) {
-				count++;
-			}
-			if (pair.getPositiveMannerTarget() && pair.getNegativeMannerSource() ||
-					pair.getNegativeMannerSource() && pair.getPositiveMannerSource()) {
-				count++;
-			}
+            }
+            if (pair.getAmplifierTarget() && pair.getDowntonerSource() ||
+                    pair.getDowntonerTarget() && pair.getAmplifierSource()) {
+                count++;
+            }
+            if (pair.getPositiveMannerTarget() && pair.getNegativeMannerSource() ||
+                    pair.getNegativeMannerSource() && pair.getPositiveMannerSource()) {
+                count++;
+            }
 
-			System.out.println("");
-		}
-		System.out.println(count);
-		return count;
-	}
+            System.out.println("");
+        }
+        System.out.println(count);
+        return count;
+    }
 
 }
--- sentence-feature-extractor/src/main/java/shiftinview/wuPalmerTest/Main.java
+++ sentence-feature-extractor/src/main/java/shiftinview/wuPalmerTest/Main.java
@@ -9,7 +9,10 @@ import shiftinview.wuPalmerTest.controllers.VerbPairsWithAllScoresController;
 import shiftinview.wuPalmerTest.models.*;
 import utils.NLPUtils;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
@@ -20,7 +23,12 @@ public class Main {
     private static void checkAccuracyOfMeasures() {
         ArrayList<VerbPairWithAllScores> verbPairsWithAllScores = VerbPairsWithAllScoresController.getAllAnnotatedVerbPairs();
 
-        double[] values = {0.75, 0.80, 0.85, 0.86, 0.87, 0.88, 0.89, 0.90, 0.95};
+        ArrayList<Double> values = new ArrayList<>();
+
+        for (int i = 75; i <= 90; i++) {
+            values.add(((double) i) / 100);
+        }
+
         for (double value : values) {
             checkSimilarityMeasureAccuracy(Constants.JIAN_CONRATH, verbPairsWithAllScores, value);
         }
